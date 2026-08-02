e4af3defb075d2b8473b349ccc92e99897d56b86
Noman-Prime <your-email@example.com>
2026-08-02 11:08:47 +0500

    Login Setup updated

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,4 +1,5 @@
 import { login } from "@/controllers/user"
+import { isAuthenticated } from "@/lib/auth"
 
 export const POST = async(req)=>{
     return login(req)
--- src/app/api/logout/route.js
+++ src/app/api/logout/route.js
@@ -1,10 +1,5 @@
 import { logOut } from "@/controllers/user"
-import { isAuthenticated } from "@/lib/auth"
 
 export const POST = async (req) => {
-    const auth = await isAuthenticated(req)
-    if (!auth.user) {
-        return auth
-    }
     return logOut()
 }
